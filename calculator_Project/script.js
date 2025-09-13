88698d2b78968653daa7a97324663b5a0b1d79c2
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-09-13 16:18:55 +0500

    Updated

--- calculator_Project/script.js
+++ calculator_Project/script.js
@@ -1,5 +1,22 @@
-let inputValue = document.getElementById("calc");
+let inputValue = document.getElementById("input-box");
 console.log(inputValue);
 
-let buttons = document.querySelector("button");
+let buttons = document.querySelectorAll("button");
 console.log(buttons);
+
+let result = "";
+
+let buttonsArr = Array.from(buttons);
+
+
+buttonsArr.forEach(buttons => {
+    buttons.addEventListener('Click', (e) =>{
+        if (e.target.innerHTML == "="){
+            result = eval(result)
+            inputValue.value = result
+        }else {
+            result += e.target.innerHTML
+            inputValue.value = result
+        }
+    })
+})
