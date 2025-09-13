11018fd8806c513b3178c4d5915e98bec713a4f9
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-09-13 18:07:48 +0500

    Updated

--- calculator_Project/script.js
+++ calculator_Project/script.js
@@ -1,22 +1,28 @@
-let inputValue = document.getElementById("input-box");
-console.log(inputValue);
+let inputValue = document.getElementById('input-box');
+// console.log(inputValue);
 
-let buttons = document.querySelectorAll("button");
-console.log(buttons);
+let buttons = document.querySelectorAll('button');
+// console.log(buttons);
 
 let result = "";
 
 let buttonsArr = Array.from(buttons);
-
+// console.log(buttonsArr);
 
 buttonsArr.forEach(buttons => {
-    buttons.addEventListener('Click', (e) =>{
-        if (e.target.innerHTML == "="){
-            result = eval(result)
+    buttons.addEventListener('click', (e) => {
+        if (e.target.innerHTML == "=") {
+            result = eval(result);
+            inputValue.value = result;
+        } else if (e.target.innerHTML == "AC") {
+            result = ""
+            inputValue.value = result;
+        } else if (e.target.innerHTML == "Del") {
+            result = result.substring(0, result.length - 1)
             inputValue.value = result
-        }else {
-            result += e.target.innerHTML
-            inputValue.value = result
+        } else {
+            result += e.target.innerHTML;
+            inputValue.value = result;
         }
     })
 })
